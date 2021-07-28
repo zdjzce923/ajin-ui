fec917f22e9b83e3d6f6ce16846476cbc4cf3caa
zdj <493904954@qq.com>
2021-07-28 15:28:23 +0800

    input基本样式

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import Button from './button.vue'
 import Icon from './icon.vue'
 import ButtonGroup from './button-group.vue'
+import GInput from './input.vue'
 Vue.component('g-button', Button)
 Vue.component('g-button-group', ButtonGroup)
+Vue.component('g-input', GInput)
 new Vue({
   el: '#app',
   data: { loading1: true, loading2: false, loading3: false },
