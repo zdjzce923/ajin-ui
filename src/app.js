b515e5f87a205db07608d19b46e4712becc53455
zdj <493904954@qq.com>
2021-07-31 14:52:45 +0800

    完成 toast 测试

--- src/app.js
+++ src/app.js
@@ -27,23 +27,35 @@ Vue.use(plugin)
 
 new Vue({
   el: '#app',
-  data: { message: 'hi' },
-  created() {
-    this.$toast('你的智商需要充值！', {
-      position: 'middle',
-      enableHtml: false,
-      closeButton: {
-        text: '已充值',
-        callback() {
-          console.log('他说已经充值智商了')
+  data: {
+    loading1: false,
+    loading2: true,
+    loading3: false,
+    message: 'hi',
+  },
+  methods: {
+    showToast1() {
+      this.showToast('top')
+    },
+    showToast2() {
+      this.showToast('middle')
+    },
+    showToast3() {
+      this.showToast('bottom')
+    },
+    showToast(position) {
+      this.$toast(`你的智商目前为 ${parseInt(Math.random() * 100)}。你的智商需要充值！`, {
+        position: 'bottom',
+        position,
+        enableHtml: false,
+        closeButton: {
+          text: '已充值',
+          callback() {
+            console.log('他说已经充值智商了')
+          },
         },
-      },
-      autoClose: false,
-      autoCloseDelay: 3,
-    })
+        autoClose: 2,
+      })
+    },
   },
 })
-import chai from 'chai'
-const expect = chai.expect
-import spies from 'chai-spies'
-chai.use(spies)
