6f17f6870ad0883f214573bf0d23eef657316c45
zdj <493904954@qq.com>
2021-08-03 16:07:24 +0800

    表驱动 添加四个可选方位

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="popover" @click="onClick" ref="popover">
-    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
+    <div
+      ref="contentWrapper"
+      class="content-wrapper"
+      v-if="visible"
+      :class="`position-${position}`"
+    >
       <slot name="content"></slot>
     </div>
     <span ref="triggerWrapper" style="display: inline-block">
@@ -16,13 +21,36 @@ export default {
       visible: false,
     };
   },
+  props: {
+    position: {
+      type: String,
+      default: "top",
+      validator(value) {
+        return ["top", "left", "right", "bottom"].indexOf(value) >= 0;
+      },
+    },
+  },
   methods: {
     positionContent() {
+      const { contentWrapper, triggerWrapper } = this.$refs;
       document.body.appendChild(this.$refs.contentWrapper);
-      let { left, top, width, height } =
-        this.$refs.triggerWrapper.getBoundingClientRect();
-      this.$refs.contentWrapper.style.left = left + scrollX + "px";
-      this.$refs.contentWrapper.style.top = top + scrollY + "px";
+      const { left, top, width, height } =
+        triggerWrapper.getBoundingClientRect();
+      const height2 = contentWrapper.getBoundingClientRect().height;
+      let positions = {
+        top: { left: left + scrollX, top: top + scrollY },
+        bottom: { left: left + scrollX, top: top + height + scrollY },
+        left: {
+          left: left + scrollX,
+          top: top + scrollY + (height - height2) / 2,
+        },
+        right: {
+          left: left + scrollX + width,
+          top: top + scrollY + (height - height2) / 2,
+        },
+      };
+      contentWrapper.style.left = positions[this.position].left + "px";
+      contentWrapper.style.top = positions[this.position].top + "px";
     },
     onClickDocument(e) {
       if (
@@ -79,8 +107,6 @@ $border-radius: 4px;
   border-radius: $border-radius;
   filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
   background: white;
-  transform: translateY(-100%);
-  margin-top: -10px;
   padding: 0.5em 1em;
   max-width: 20em;
   word-break: break-all;
@@ -92,15 +118,67 @@ $border-radius: 4px;
     width: 0;
     height: 0;
     position: absolute;
-    left: 10px;
   }
-  &::before {
-    border-top-color: black;
-    top: 100%;
+  &.position-top {
+    transform: translateY(-100%);
+    margin-top: -10px;
+    &::before {
+      border-top-color: black;
+      top: 100%;
+      left: 10px;
+    }
+    &::after {
+      border-top-color: white;
+      top: calc(100% - 1px);
+      left: 10px;
+    }
   }
-  &::after {
-    border-top-color: white;
-    top: calc(100% - 1px);
+  &.position-bottom {
+    margin-top: 10px;
+    &::before {
+      border-bottom-color: black;
+      bottom: 100%;
+      left: 10px;
+    }
+    &::after {
+      border-bottom-color: white;
+      bottom: calc(100% - 1px);
+      left: 10px;
+    }
+  }
+  &.position-left {
+    transform: translateX(-100%);
+    margin-left: -10px;
+    &::before,
+    &::after {
+      transform: translateY(-50%);
+      top: 50%;
+    }
+    &::before {
+      border-left-color: black;
+      left: 100%;
+    }
+    &::after {
+      border-left-color: white;
+      left: calc(100% - 1px);
+    }
+  }
+  &.position-right {
+    // transform: translateX(50%);
+    margin-left: 10px;
+    &::before,
+    &::after {
+      transform: translateY(-50%);
+      top: 50%;
+    }
+    &::before {
+      border-right-color: black;
+      right: 100%;
+    }
+    &::after {
+      border-right-color: white;
+      right: calc(100% - 1px);
+    }
   }
 }
 </style>
